0eb6ebee91eccdeaea7e466dc99a5c2b85fce4de
Manish Devrani <manishdevrani777@gmail.com>
2022-05-09 03:34:42 +0530

    added tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      <h1>manish</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 };
