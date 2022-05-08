5a72b260982c84cc0274d034718798d84489cf7c
Manish Devrani <manishdevrani777@gmail.com>
2022-05-09 03:29:48 +0530

    added eslint prettier absolute paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <h1>manish</h1>
     </div>
   );
-}
+};
 
 export default App;
